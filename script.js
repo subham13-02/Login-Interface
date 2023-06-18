7a69c979aa9a98ef80e1aa13aefe2d2098e66d6b
subham13-02 <sahu21019@gmail.com>
2023-06-19 04:20:40 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,10 @@
+
+function callme(){
+    const modalElement = document.getElementById("modal");
+    modalElement.style.display="flex";
+
+}
+function call(){
+    const modalElement = document.getElementById("modal");
+    modalElement.style.display="none";
+}
